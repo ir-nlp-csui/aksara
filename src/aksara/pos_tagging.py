83de6d94b2364b6ab0d8ce926bd74fde8d0735d7
ainul.malik <ainul.malik@ui.ac.id>
2023-03-03 16:36:24 +0700

    Membuat implementasi POS Tagging
    Former-commit-id: 13108398a1e52a0fec6963452e06dc4171e21a9a

--- src/aksara/pos_tagging.py
+++ src/aksara/pos_tagging.py
@@ -1,3 +1,6 @@
+import os
+from conllu import parse
+
 # POS Tagging Satu Kata
 # TODO
 
@@ -9,3 +12,49 @@
 
 # POS Tagging File
 # TODO
+
+
+def pos_tagging(
+    input_text: str, input_type: str = "s", informal_bool: bool = False
+) -> list[list[list[str]]]:
+    input_type = "-f" if input_type.lower() in ["file", "f"] else "-s"
+    informal = " --informal" if informal_bool else ""
+
+    cmd = f"python3 src/aksara.py --postag{informal} {input_type} '{input_text}'"
+
+    sentences = os.popen(cmd).read()
+
+    if input_type == "-f":
+        sentences = sentences[21:]
+
+    sentences = parse(sentences)
+
+    result = []
+
+    for sentence in sentences:
+        sentence_list = []
+        for word in sentence:
+            sentence_list.append([word["form"], word["lemma"]])
+        result.append(sentence_list)
+
+    return result
+
+
+if __name__ == "__main__":
+    # print(
+    #     pos_tagging(
+    #         "/Users/malikismail/Library/CloudStorage/OneDrive-UNIVERSITASINDONESIA/Documents/Uni/Sem 6/PPL/NLP Aksara/nlp-aksara/src/input_example.txt",
+    #         "f",
+    #     )
+    # )
+    print(
+        pos_tagging(
+            "/Users/malikismail/Library/CloudStorage/OneDrive-UNIVERSITASINDONESIA/Documents/Uni/Sem 6/PPL/NLP Aksara/nlp-aksara/tests/testinput.txt",
+            "f",
+        )
+    )
+    # print(
+    #     pos_tagging(
+    #         '"Meski kebanyakan transisi digital yang terjadi di Amerika Serikat belum pernah terjadi sebelumnya, transisi kekuasaan yang damai tidaklah begitu," tulis asisten khusus Obama, Kori Schulman di sebuah postingan blog pada hari Senin.'
+    #     )
+    # )
--- tests/test_pos_tagging.py
+++ tests/test_pos_tagging.py
@@ -1,14 +1,49 @@
 import unittest
-from aksara.pos_tagging import *
+from aksara.pos_tagging import pos_tagging
 
-# POS Tagging Satu Kata
-# TODO
 
-# POS Tagging Satu Kalimat
-# TODO
+class POSTaggingTest(unittest.TestCase):
+    # POS Tagging Satu Kata
+    # TODO
 
-# POS Tagging Multi-Kalimat
-# TODO
+    # POS Tagging Satu Kalimat
+    # TODO
 
-# POS Tagging File
-# TODO
+    # POS Tagging Multi-Kalimat
+    # TODO
+
+    # POS Tagging File
+    # TODO
+
+    def test_pos_tagging_file(self):
+        testcase = "Pengeluaran baru ini dipasok oleh rekening bank gemuk Clinton. Uang yang hilang pada tahun itu sangat banyak."
+        expected = [
+            [
+                ["Pengeluaran", "NOUN"],
+                ["baru", "ADJ"],
+                ["ini", "DET"],
+                ["dipasok", "VERB"],
+                ["oleh", "ADP"],
+                ["rekening", "NOUN"],
+                ["bank", "NOUN"],
+                ["gemuk", "ADJ"],
+                ["Clinton", "PROPN"],
+                [".", "PUNCT"],
+            ],
+            [
+                ["Uang", "NOUN"],
+                ["yang", "SCONJ"],
+                ["hilang", "ADJ"],
+                ["pada", "ADP"],
+                ["tahun", "NOUN"],
+                ["itu", "DET"],
+                ["sangat", "ADV"],
+                ["banyak", "DET"],
+                [".", "PUNCT"],
+            ],
+        ]
+
+        self.assertEqual(
+            pos_tagging(testcase, "f"),
+            expected,
+        )
