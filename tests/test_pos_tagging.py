9e652ff359ab074e73ba02f08361bd9c26076c69
ainul.malik <ainul.malik@ui.ac.id>
2023-03-11 06:52:06 +0700

    [REFACTOR] Menyesuaikan file test dengan refactor
    Former-commit-id: 0dfcc603a3fe179548b310c451028e7d77e02d2d

--- tests/test_pos_tagging.py
+++ tests/test_pos_tagging.py
@@ -1,83 +1,112 @@
 import unittest
 import os
+from unittest.mock import Mock
 from src.aksara.pos_tagging import *
 
 
-class POSTaggingTest(unittest.TestCase):
-    # POS Tagging Satu Kata
-    # TODO
-
-    # POS Tagging Satu Kalimat
-    # TODO
-
-    # POS Tagging Multi-Kalimat
-    # TODO
-
-    def test_pos_tagging_string(self):
-        testcase = "Pengeluaran baru ini dipasok oleh rekening bank gemuk Clinton. Uang yang hilang pada tahun itu sangat banyak."
-        expected = [
-            [
-                ["Pengeluaran", "NOUN"],
-                ["baru", "ADJ"],
-                ["ini", "DET"],
-                ["dipasok", "VERB"],
-                ["oleh", "ADP"],
-                ["rekening", "NOUN"],
-                ["bank", "NOUN"],
-                ["gemuk", "ADJ"],
-                ["Clinton", "PROPN"],
-                [".", "PUNCT"],
-            ],
-            [
-                ["Uang", "NOUN"],
-                ["yang", "SCONJ"],
-                ["hilang", "ADJ"],
-                ["pada", "ADP"],
-                ["tahun", "NOUN"],
-                ["itu", "DET"],
-                ["sangat", "ADV"],
-                ["banyak", "DET"],
-                [".", "PUNCT"],
-            ],
-        ]
-
-        self.assertEqual(
-            pos_tagging(testcase, "s"),
-            expected,
-        )
+class TestPOSTaggingFile(unittest.TestCase):
+    # mock = Mock()
 
     # POS Tagging File
 
     def test_pos_tagging_file(self):
-        dirpath, _ = os.path.split(os.path.realpath(__file__))
-        testcase = os.path.join(dirpath, "testinput.txt")
+        testcase = os.path.join(os.path.dirname(__file__), "testinput.txt")
+
         expected = [
             [
-                ["Pengeluaran", "NOUN"],
-                ["baru", "ADJ"],
-                ["ini", "DET"],
-                ["dipasok", "VERB"],
-                ["oleh", "ADP"],
-                ["rekening", "NOUN"],
-                ["bank", "NOUN"],
-                ["gemuk", "ADJ"],
-                ["Clinton", "PROPN"],
-                [".", "PUNCT"],
+                ("Pengeluaran", "NOUN"),
+                ("baru", "ADJ"),
+                ("ini", "DET"),
+                ("dipasok", "VERB"),
+                ("oleh", "ADP"),
+                ("rekening", "NOUN"),
+                ("bank", "NOUN"),
+                ("gemuk", "ADJ"),
+                ("Clinton", "PROPN"),
+                (".", "PUNCT"),
             ],
             [
-                ["Uang", "NOUN"],
-                ["yang", "SCONJ"],
-                ["hilang", "ADJ"],
-                ["pada", "ADP"],
-                ["tahun", "NOUN"],
-                ["itu", "DET"],
-                ["sangat", "ADV"],
-                ["banyak", "DET"],
-                [".", "PUNCT"],
+                ("Uang", "NOUN"),
+                ("yang", "SCONJ"),
+                ("hilang", "ADJ"),
+                ("pada", "ADP"),
+                ("tahun", "NOUN"),
+                ("itu", "DET"),
+                ("sangat", "ADV"),
+                ("banyak", "DET"),
+                (".", "PUNCT"),
             ],
         ]
 
+        # Correct Input
+        self.assertEqual(pos_tagging_file(testcase), expected)
+
+        # Correct Input with Informal
+        self.assertEqual(pos_tagging_file(testcase, True), expected)
+
+    def test_pos_tagging_file_informal(self):
+        testcase = os.path.join(os.path.dirname(__file__), "testinput_informal.txt")
+        expected = [
+            [
+                ("Gue", "PRON"),
+                ("gatau", "X"),
+                ("maksud", "NOUN"),
+                ("lu", "PRON"),
+                ("apa", "PRON"),
+                (",", "PUNCT"),
+                ("tapi", "CCONJ"),
+                ("kenyataannya", "_"),
+                ("kenyataan", "NOUN"),
+                ("nya", "PRON"),
+                ("ngga", "X"),
+                ("begitu", "DET"),
+                (".", "PUNCT"),
+            ],
+            [
+                ("Cuma", "NOUN"),
+                ("dia", "PRON"),
+                ("doang", "ADV"),
+                ("yang", "SCONJ"),
+                ("ga", "PART"),
+                ("tau", "VERB"),
+                ("itu", "DET"),
+                ("apaan", "X"),
+                (".", "PUNCT"),
+            ],
+        ]
+
+        # Correct Informal
+        self.assertEqual(pos_tagging_file(testcase, True), expected)
+
+        # Correct Formal
         self.assertEqual(
-            pos_tagging(testcase, "f"),
-            expected,
+            pos_tagging_file(testcase),
+            [
+                [
+                    ("Gue", "PROPN"),
+                    ("gatau", "X"),
+                    ("maksud", "NOUN"),
+                    ("lu", "X"),
+                    ("apa", "PRON"),
+                    (",", "PUNCT"),
+                    ("tapi", "CCONJ"),
+                    ("kenyataannya", "_"),
+                    ("kenyataan", "NOUN"),
+                    ("nya", "PRON"),
+                    ("ngga", "X"),
+                    ("begitu", "DET"),
+                    (".", "PUNCT"),
+                ],
+                [
+                    ("Cuma", "NOUN"),
+                    ("dia", "PRON"),
+                    ("doang", "X"),
+                    ("yang", "SCONJ"),
+                    ("ga", "X"),
+                    ("tau", "VERB"),
+                    ("itu", "DET"),
+                    ("apaan", "X"),
+                    (".", "PUNCT"),
+                ],
+            ],
         )
