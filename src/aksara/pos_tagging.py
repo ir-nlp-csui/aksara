7307d00cd1b0e58775ad6f130a314afec6070616
Rizki Kurniawan <rizki.kurniawan02@ui.ac.id>
2023-03-15 14:50:21 +0700

    Implementasi: PBI-3-POS Tagging Output ke File
    Former-commit-id: 54ca8077364c5c5b79ffb6f371f88788d5cb3fa1

--- src/aksara/pos_tagging.py
+++ src/aksara/pos_tagging.py
@@ -1,6 +1,6 @@
 """This file contains various POS tagging functions"""
 
-from typing import List, Tuple
+from typing import List, Tuple, Literal
 from tqdm import tqdm
 import re
 import os
@@ -50,11 +50,11 @@ def pos_tagging_one_sentence(input_text: str, is_informal: bool = False) -> list
 
 
 # POS Tagging Multi-Kalimat
-def tag_multi_sentences(
-        sentences: str,
-        is_informal: bool = False,
-        sep_regex: str = '([.!?]+[\s])'
-) -> List[List[Tuple[str, str]]]:
+# TODO
+
+def tag_multi_sentences(sentences: str, is_informal: bool= False,
+                        sep_regex: str = '([.!?]+[\\\s])') -> List[List[Tuple[str, str]]]:
+
     sentences = sentences.strip()
 
     if sentences == '':
@@ -62,28 +62,15 @@ def tag_multi_sentences(
 
     result: List[List[Tuple[str, str]]] = []
 
-    splitted_sentences = re.split(codecs.decode(sep_regex, 'unicode_escape'), sentences)
-    sentence_list = []
-    for i in range(len(splitted_sentences)):
-        if i % 2 == 0:
-            sentence_list.append(
-                splitted_sentences[i]
-                + (splitted_sentences[i + 1]
-                   if i != len(splitted_sentences) - 1
-                   else ""))
+    sentence_list = __split_sentence(sentences, sep_regex)
 
     default_analyzer = __get_default_analyzer()
     default_dependency_parser = __get_default_dependency_parser()
     for sentence in sentence_list:
-        analyzed_sentence = analyze_sentence(
-            sentence,
-            default_analyzer,
-            default_dependency_parser,
-            v1=False,
-            lemma=False,
-            postag=True,
-            informal=is_informal
-        )
+        analyzed_sentence = analyze_sentence(sentence, default_analyzer,
+                                             default_dependency_parser, v1=False,
+                                             lemma=False, postag=True,
+                                             informal=is_informal)
 
         temp_result = []
         for token_with_tag_str in analyzed_sentence.split('\n'):
@@ -158,3 +145,111 @@ def __get_default_analyzer() -> BaseAnalyzer:
 
 def __get_default_dependency_parser() -> DependencyParser:
     return DependencyParser()
+
+
+# POS Tagging File
+# TODO
+
+
+# POS Tagging output file
+def tag_then_save_to_file(text: str, file_path: str,
+                          sep_regex: str= '([.!?]+[\\\s])',
+                          write_mode: Literal['x', 'a', 'w']='w',
+                          is_informal: bool=False) -> bool:
+    """
+    performs pos tagging on the text, then save the result in the file specified by file_path 
+
+    parameters
+    ----------
+    text: str
+        the text that will be analyzed
+
+    file_path: str
+        path to the file where the pos tagging result will be saved to
+
+    sep_regex: str
+        regex rule that specify end of sentence
+        
+    write_mode: str, ['x', 'a', 'w'], default to 'w'
+        mode while writing on the file specified by file_path
+
+        'x': create the specified file, throws error FileExistsError if already exists
+        'a': append the pos tagging result at the end of the file, 
+                create the specified file if not exists
+        'w': overwrite the current file content with the pos tagging result, 
+                create the specified file if not exists
+
+        NOTE:
+        - if write_mode not in ['x', 'a', 'w'] will throws ValueError
+        - 'a' write_mode will add '\n\n' before the pos tagging result.
+        - If you plan to use 'a' write_mode in a file that already has 
+            pos taging result on connlu format, the sent_id between 
+            the old (already in the file) and new pos tag result will not ne in sequence.
+
+    is_informal: bool
+        tell aksara to treat text as informal one, default to False
+            
+    return
+    ------
+    ReturnType: bool
+        will return true if succesfully pos tag and save the result on the given file_path
+    """
+
+    all_write_modes = ['x', 'a', 'w']
+
+    if write_mode not in all_write_modes:
+        raise ValueError(f"write_mode must be in {all_write_modes}")
+
+    sentence_list = __split_sentence(text, sep_regex)
+
+    result_list: List[List[Tuple[str, str]]] = tag_multi_sentences(text,
+                                                                   sep_regex=sep_regex,
+                                                                   is_informal=is_informal)
+
+    header_format = "# sent_id = {}\n# text = {}"
+
+    with open(file_path, write_mode, encoding="utf-8") as output_file:
+
+        if write_mode == 'a' and len(result_list) != 0:
+            output_file.writelines('\n\n')
+
+        for i, token_with_tag in enumerate(result_list):
+            output_file.writelines(header_format.format(str(i + 1), sentence_list[i]))
+
+            for idx, (token, tag) in enumerate(token_with_tag):
+                output_file.writelines(f"\n{idx + 1}\t{token}\t{tag}")
+
+            if i < len(result_list) - 1:   # don't add \n at the end of the file
+                output_file.writelines('\n\n')
+
+    return True
+
+def __split_sentence(text: str, sep_regex: str='([.!?]+[\\\s])') -> List[str]:
+    """
+    this method will split a multi sentences text based on separator regex (sep_regex)
+    
+    paramters
+    ---------
+
+    text: str
+        sentences that will be splitted
+
+    sep_regex: str
+        regex rule that determine the points where the text will be splitted at.
+    
+    return
+    ------
+    ReturnType: List of string
+        splitted text
+    """
+
+    splitted_sentences = re.split(codecs.decode(sep_regex, 'unicode_escape'), text)
+    sentence_list = []
+    for i in range(len(splitted_sentences)):
+        if i % 2 == 0:
+            sentence_with_end_mark = splitted_sentences[i] + \
+                (splitted_sentences[i + 1] if i != len(splitted_sentences) - 1 else "")
+
+            sentence_list.append(sentence_with_end_mark)
+
+    return sentence_list
