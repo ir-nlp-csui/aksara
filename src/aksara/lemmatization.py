4dcd07815c745f4646fa1a20f4f7b88fa4f4d3be
Debra Mazaya <debra.mazaya@ui.ac.id>
2023-03-26 11:30:35 +0700

    Attempted to fix code coverage
    Former-commit-id: 7ebedb1e8c5847752eb2f468d95d86728ad9530f

--- src/aksara/lemmatization.py
+++ src/aksara/lemmatization.py
@@ -5,7 +5,7 @@ from aksara.analyzer import BaseAnalyzer
 from dependency_parsing.core import DependencyParser
 
 # Lemmatization Satu Kata
-def lemmatization_list(word_input: str, is_informal: bool = False) -> str:
+def lemmatization_one_word(word_input: str, is_informal: bool = False) -> str:
     """This function receives a certain word and returns lemmatization result of the word"""
 
     analyzer = __get_default_analyzer()
@@ -14,6 +14,7 @@ def lemmatization_list(word_input: str, is_informal: bool = False) -> str:
     temp_result = analyze_sentence(text=word_input, analyzer=analyzer, dependency_parser=dependency_parser, v1=False,
                                    lemma=True, postag=False, informal=is_informal)
 
+    _, word, lemma = temp_result.split("\t")
     result = lemma
 
     return result
@@ -41,11 +42,7 @@ def lemmatization_list(list_word: list, is_informal: bool = False) -> list[tuple
 
 
 def __get_default_analyzer():
-    current_module_path = os.path.realpath(__file__)
-    current_dir_path, _ = os.path.split(current_module_path)
-    src_path, _ = os.path.split(current_dir_path)
-
-    bin_path = os.path.join(src_path, 'bin', 'aksara@v1.2.0.bin')
+    bin_path = os.path.join(os.path.dirname(__file__), "bin", "aksara@v1.2.0.bin")
     return BaseAnalyzer(bin_path)
 
 
