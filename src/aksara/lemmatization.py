f90ba93c237d3c7fade57ec5fee4b6ce5348ee09
Debra Mazaya <debra.mazaya@ui.ac.id>
2023-04-07 21:36:01 +0700

    Finalized feature
    Former-commit-id: 3b030b3d36dffb5db116b866a053a4fa5f1e220c

--- src/aksara/lemmatization.py
+++ src/aksara/lemmatization.py
@@ -6,36 +6,36 @@ from aksara.analyzer import BaseAnalyzer
 from dependency_parsing.core import DependencyParser
 
 # Lemmatization Satu Kata
-def lemmatization(input_text: Union[list, str], is_informal: bool = False) -> str:
+def lemmatization(input_text: Union[list, str], is_informal: bool = False) -> Union[list,str]:
     """This function receives a certain string or a list 
     and returns lemmatization result of the said word/list"""
 
-    if type(input_text) is str:
-        processed_input = input_text.strip()
-        if input_text is '':
+    if isinstance(input_text) is str:
+        if input_text == '':
             return ''
+        processed_input = input_text.strip()
     else:
-        processed_input = ' '.join(input_text)
         if input_text == []:
             return []
-        
+        processed_input = ' '.join(input_text)
         result = []
 
     analyzer = __get_default_analyzer()
     dependency_parser = __get_default_dependency_parser()
 
-    temp_result = analyze_sentence(text=processed_input, 
-                                   analyzer=analyzer, 
-                                   dependency_parser=dependency_parser, 
-                                   v1=False, 
-                                   lemma=True, 
-                                   postag=False, 
+    temp_result = analyze_sentence(text=processed_input,
+                                   analyzer=analyzer,
+                                   dependency_parser=dependency_parser,
+                                   v1=False,
+                                   lemma=True,
+                                   postag=False,
                                    informal=is_informal)
 
-    if result is not None:
+    if isinstance(input_text) is list:
         for line in temp_result.split("\n"):
             _, word, lemma = line.split("\t")
             result.append((word, lemma))
+            #print(result)
         return result
 
     _, _, result = temp_result.split("\t")
