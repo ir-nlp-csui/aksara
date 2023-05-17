88a53ab403edb3f775838532aa673afc669aff3c
rizki-kurniawan <rizki.kurniawan02@ui.ac.id>
2023-05-17 17:19:19 +0700

    [RED] fix bug tokenizer multiword token
    Former-commit-id: fbcd24b9ec417a68cd54e07e735b9b49ff906ad5

--- tests/tokenizer_test/test_base_tokenize.py
+++ tests/tokenizer_test/test_base_tokenize.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from unittest.mock import patch, Mock
 
 from tests.tokenizer_test.tokenizer_test_setup import TokenizerTestSetUp
@@ -14,31 +15,31 @@ class BaseTokenizerTest(TokenizerTestSetUp):
         super().setUp()
         self.tokenizer = BaseTokenizer()
 
-    @patch.object(_internal_tokenizer.BaseTokenizer, 'tokenize')
-    def test_basetokenizer_class_method_called_once(self, tokenizer_mock: Mock):
-        tokenizer_mock.return_value = ["sebuah", "string"], [False, False]
-        self.tokenizer.tokenize("sebuah string")
-        tokenizer_mock.assert_called_once_with("sebuah string")
-
     def test_base_tokenize_should_return_word_list(self):
-        expected = ["saya", "makan"]
+        expected = [["saya", "makan"]]
         self.assertListEqual(expected, self.tokenizer.tokenize("saya makan"))
 
     def test_empty_string_input(self):
-        self.assertEqual([], self.tokenizer.tokenize(""))
+        self.assertListEqual([], self.tokenizer.tokenize(""))
 
     def test_input_that_contains_whitespace_only(self):
-        self.assertEqual([], self.tokenizer.tokenize("    "))
+        self.assertListEqual([], self.tokenizer.tokenize("    "))
 
     def test_one_sentence_tokenizer(self):
         result = self.tokenizer.tokenize(self.one_sentence)
-        self.assertEqual(self.expected_one_sentence, result)
+        self.assertListEqual(self.expected_one_sentence, result)
 
     def test_multiple_sentence_tokenizer(self):
         result = self.tokenizer.tokenize(self.multiple_sentence)
-        self.assertEqual(self.expected_multiple_sentence, result)
+        self.assertListEqual(self.expected_multiple_sentence, result)
+
+    def test_multiple_sentence_tokenizer_no_sentence_split(self):
+        result = self.tokenizer.tokenize(self.multiple_sentence, ssplit=False)
+        
+        reduced_list = [[token for tokens in self.expected_multiple_sentence for token in tokens]]
+        self.assertListEqual(reduced_list, result)
 
     def test_input_contains_multiword_token(self):
-        expected_multiword_input = ["biarlah", "saja"]
+        expected_multiword_input = [["biarlah", "saja"]]
         result = self.tokenizer.tokenize(self.multiword_input)
-        self.assertEqual(expected_multiword_input, result)
+        self.assertListEqual(expected_multiword_input, result)
--- tests/tokenizer_test/test_multiword_tokenize.py
+++ tests/tokenizer_test/test_multiword_tokenize.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from unittest.mock import patch, Mock
 from tests.tokenizer_test.tokenizer_test_setup import TokenizerTestSetUp
 
@@ -11,7 +12,7 @@ class MultiWordTokenizerTest(TokenizerTestSetUp):
 
     def setUp(self) -> None:
         super().setUp()
-        self.expected_multiword_input = ["biar", "lah", "saja"]
+        self.expected_multiword_input = [["biar", "lah", "saja"]]
         self.tokenizer = MultiwordTokenizer()
 
     @patch(
@@ -47,6 +48,12 @@ class MultiWordTokenizerTest(TokenizerTestSetUp):
     def test_multiple_sentence_tokenizer(self):
         result = self.tokenizer.tokenize(self.multiple_sentence)
         self.assertEqual(self.expected_multiple_sentence, result)
+
+    def test_multiple_sentence_tokenizer_no_sentence_split(self):
+        result = self.tokenizer.tokenize(self.multiple_sentence, ssplit=False)
+        
+        reduced_list = [token for tokens in self.expected_multiple_sentence for token in tokens]
+        self.assertEqual([reduced_list], result)
 
     def test_input_contains_multiword_token(self):
         result = self.tokenizer.tokenize(self.multiword_input)
